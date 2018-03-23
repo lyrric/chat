7afb186a241fdbc28227c9f8400ef09370a52cb5
lyrric <xx20510@163.com>
2018-03-23 20:31:59 +0800

    都什么年代了,还用啥配置文件啊,注解大法好

--- client/src/main/java/com/play001/chat/client/Application.java
+++ client/src/main/java/com/play001/chat/client/Application.java
@@ -1,13 +1,17 @@
 package com.play001.chat.client;
 
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 /**
  * spring 启动
  */
+
+@ComponentScan(basePackages = "com.play001.chat")
 public class Application {
     public static void main(String[] args) {
-        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("Spring.xml");
+        new AnnotationConfigApplicationContext(Application.class);
     }
 }
