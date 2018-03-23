3a445156a049f71699044f85e94daf165eb4ccf8
lyrric <xx20510@163.com>
2018-03-23 20:28:23 +0800

    都什么年代了,还用啥配置文件啊,注解大法好

--- server/src/main/java/com/play001/chat/server/Application.java
+++ server/src/main/java/com/play001/chat/server/Application.java
@@ -1,19 +1,21 @@
 package com.play001.chat.server;
 
 import com.play001.chat.server.handler.Hello;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
-import org.springframework.stereotype.Component;
-
-import javax.annotation.PostConstruct;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
 /**
  * spring 启动
  */
+
+@ComponentScan(basePackages = "com.play001.chat")
 public class Application {
+
+
     public static void main(String[] args) {
-        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("Spring.xml");
+        new AnnotationConfigApplicationContext(Application.class);
     }
 }
